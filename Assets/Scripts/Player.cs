820b89796556771ccbaee0bce8b4caa11b047e82
Geoffrey Mugford <geoffrey.mugford@gmail.com>
2020-06-07 15:53:09 +0100

    Basic player movement + folder structure

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float walkSpeed = 5f;
+
+    Rigidbody2D myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float xThrow = Input.GetAxis("Horizontal") * walkSpeed;
+        float yThrow = Input.GetAxis("Vertical") * walkSpeed;
+        Vector2 walkVelocity = new Vector2(xThrow, yThrow);
+
+        myRigidbody.velocity = walkVelocity;
+    }
+}
