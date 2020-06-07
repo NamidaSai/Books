71ea805acd185987fa491402436bc1d227c84184
Geoffrey Mugford <geoffrey.mugford@gmail.com>
2020-06-07 16:34:13 +0100

    Implemented basic grab mechanic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,16 +6,21 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float walkSpeed = 5f;
 
+    [SerializeField] Transform grabbedObject = null;
+
     Rigidbody2D myRigidbody;
+    BoxCollider2D myGrabCollider;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myGrabCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
         Move();
+        Grab();
     }
 
     private void Move()
@@ -25,5 +30,20 @@ public class Player : MonoBehaviour
         Vector2 walkVelocity = new Vector2(xThrow, yThrow);
 
         myRigidbody.velocity = walkVelocity;
+    }
+
+    private void Grab()
+    {
+        bool playerIsInGrabDistance = myGrabCollider.IsTouchingLayers(LayerMask.GetMask("Moveable Objects"));
+        if (Input.GetKey(KeyCode.Space) && playerIsInGrabDistance)
+        {
+            float distance = Vector2.Distance(grabbedObject.position, transform.position);
+
+            if (distance != 1f)
+            {
+                distance = 1f;
+                grabbedObject.position = (grabbedObject.position - transform.position).normalized * distance + transform.position;
+            }
+        }
     }
 }
