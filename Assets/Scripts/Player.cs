13283e6ea767255ae0aca05197e1ad8308b31239
Geoffrey Mugford <geoffrey.mugford@gmail.com>
2020-06-09 11:06:57 +0100

    Imported player sprite and set up walk animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,16 +10,19 @@ public class Player : MonoBehaviour
 
     Rigidbody2D myRigidbody;
     BoxCollider2D myGrabCollider;
+    Animator myAnimator;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myGrabCollider = GetComponent<BoxCollider2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Move();
+        FlipSprite();
         Grab();
     }
 
@@ -30,6 +33,17 @@ public class Player : MonoBehaviour
         Vector2 walkVelocity = new Vector2(xThrow, yThrow);
 
         myRigidbody.velocity = walkVelocity;
+        bool playerHasHorizontalSpeed = (Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon);
+        myAnimator.SetBool("isWalking", playerHasHorizontalSpeed);
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = (Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon);
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(myRigidbody.velocity.x), 1f, 1f);
+        }
     }
 
     private void Grab()
