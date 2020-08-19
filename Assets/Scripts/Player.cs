d981423cb10bfbfec0c77a023ba598815faeb458
NamidaSai <timeveilprod@gmail.com>
2020-08-19 16:12:03 +0100

    Player Click Interaction

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            InteractWithObject();
+        }
+    }
+
+    private void InteractWithObject()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(GetMouseRay(), Vector2.zero);
+
+        if (hit.collider.tag == "Interactable")
+        {
+            Debug.Log(hit.collider.gameObject.name + " has been triggered.");
+        }
+    }
+    
+    private static Vector2 GetMouseRay()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
--- Assets/Z First Prototype/Scripts/PlayerPrototype.cs
+++ Assets/Z First Prototype/Scripts/PlayerPrototype.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerPrototype : MonoBehaviour
 {
     [SerializeField] float walkSpeed = 5f;
 
