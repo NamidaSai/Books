cd75427b3c5a6d217cab9fd3dcc80263b0bb13e4
NamidaSai <timeveilprod@gmail.com>
2020-08-28 13:24:38 +0100

    Replaced symbols by armenian & level controller

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    bool codeIsDecrypted = false;
+
+    public bool GetCodeIsDecrypted()
+    {
+        return codeIsDecrypted;
+    }
+
+    public void SetCodeIsDecrypted(bool isDecrypted)
+    {
+        codeIsDecrypted = isDecrypted;
+    }
+}
